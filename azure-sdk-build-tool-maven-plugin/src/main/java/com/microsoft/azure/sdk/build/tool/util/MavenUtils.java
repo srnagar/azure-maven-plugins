17ee158b01b1f5828bfce0acaa281709ae631c51
flanker32 <hanli@microsoft.com>
2025-06-24 14:26:57 +0800

    Fix compile issue

--- azure-sdk-build-tool-maven-plugin/src/main/java/com/microsoft/azure/sdk/build/tool/util/MavenUtils.java
+++ azure-sdk-build-tool-maven-plugin/src/main/java/com/microsoft/azure/sdk/build/tool/util/MavenUtils.java
@@ -11,6 +11,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
